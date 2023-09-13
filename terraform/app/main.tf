3e453800890da7b6e5858d264030c59d7df7d653
Samour <aaron.burke.cpp@gmail.com>
2023-09-13 15:50:41 +1000

    Pull Terraform resources into their own file

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -18,26 +18,3 @@ terraform {
 provider "aws" {
   region  = "ap-southeast-2"
 }
-
-resource "aws_s3_bucket" "app_artifacts" {
-  bucket = "award-calculator.ellie.aburke.me"
-  tags = {
-    "Name" = "Award Calculator FE app artifacts"
-    "Environment" = "Prod"
-    "Project" = "award-calculator"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "app_artifacts" {
-  bucket = aws_s3_bucket.app_artifacts.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "app_artifacts" {
-  depends_on = [aws_s3_bucket_ownership_controls.app_artifacts]
-
-  bucket = aws_s3_bucket.app_artifacts.id
-  acl    = "private"
-}
