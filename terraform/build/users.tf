4c5ad1a4427cd6fb6322c1380ab9f2a71d634c4a
Samour <aaron.burke.cpp@gmail.com>
2023-09-14 07:56:02 +1000

    Add terraform init to workflow
    Also grant CI user access to build bucket

--- terraform/build/users.tf
+++ terraform/build/users.tf
@@ -7,6 +7,22 @@ resource "aws_iam_user" "ci_user" {
 }
 
 data "aws_iam_policy_document" "ci_policy" {
+  // TF state bucket
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::build.aburke.me",
+      "arn:aws:s3:::build.aburke.me/*"
+    ]
+  }
+
+  // Award calculator app bucket
   statement {
     effect    = "Allow"
     actions   = [
