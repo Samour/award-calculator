c4f0986357b1237423ef9a58d67c7c73522939ff
Samour <aaron.burke.cpp@gmail.com>
2023-09-15 02:00:37 +1000

    Add "Compute Pay" button which triggers validation

--- award-calculator-fe/src/Screens/ShiftEntry/index.tsx
+++ award-calculator-fe/src/Screens/ShiftEntry/index.tsx
@@ -1,6 +1,19 @@
+import { useShiftTableValidator } from 'services/ShiftTableValidator';
 import { ShiftTable } from './ShiftTable';
 
 export const ShiftEntry = (): JSX.Element => {
+  const shiftTableValidator = useShiftTableValidator();
+
+  const onComputePayClick = () => {
+    if (shiftTableValidator.validateShiftRows()) {
+      window.alert('Data entry is valid - I would proceed to calculation now, except that\'s not built yet.');
+    } else {
+      setTimeout(() =>
+        window.alert('Data entry is not valid. Fix data before proceeding.'),
+        5);
+    }
+  };
+
   return (
     <>
       <div className="row">
@@ -11,6 +24,12 @@ export const ShiftEntry = (): JSX.Element => {
       <div className="row">
         <div className="twelve columns">
           <ShiftTable />
+        </div>
+      </div>
+      <div className="row">
+        <div className="twelve columns">
+          <button className="button-primary u-pull-right"
+            onClick={onComputePayClick}>Compute Pay</button>
         </div>
       </div>
     </>
--- award-calculator-fe/src/services/ShiftTableValidator.ts
+++ award-calculator-fe/src/services/ShiftTableValidator.ts
@@ -1,6 +1,10 @@
-import { WorkerShiftRow } from 'models/inputs/table';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch, createSelector } from '@reduxjs/toolkit';
+import { setCellValidationMessages } from 'store/reducers/shiftEntry';
+import { WorkerShiftColumnName, WorkerShiftRow } from 'models/inputs/table';
 import { WorkerCode } from 'models/inputs/worker';
 import { MonetaryAmount } from 'models/money';
+import { AppState } from 'models/store';
 import { ValidatedWorkerShiftRow } from 'models/store/shiftEntry';
 
 interface WorkerDetails {
@@ -11,52 +15,43 @@ interface WorkerDetails {
 }
 
 export class ShiftTableValidator {
-  validateShiftRows(shifts: WorkerShiftRow[]): ValidatedWorkerShiftRow[] {
+
+  constructor(private readonly dispatch: Dispatch, private readonly shifts: WorkerShiftRow[]) { }
+
+  validateShiftRows(): boolean {
     const encounteredWorkers: Map<WorkerCode, WorkerDetails> = new Map();
 
-    return shifts.map((shift) => {
-      return {
-        employeeCode: {
-          value: shift.employeeCode,
-          failureMessages: validateEmployeeCode(shift.employeeCode),
-        },
-        lastName: {
-          value: shift.lastName,
-          failureMessages: validateLastName(shift.lastName),
-        },
-        firstName: {
-          value: shift.firstName,
-          failureMessages: validateFirstName(shift.firstName),
-        },
-        basePayRate: {
-          value: shift.basePayRate,
-          failureMessages: [],
-        },
-        shiftStartDate: {
-          value: shift.shiftStartDate,
-          failureMessages: [],
-        },
-        shiftStartTime: {
-          value: shift.shiftStartTime,
-          failureMessages: [],
-        },
-        shiftEndTime: {
-          value: shift.shiftEndTime,
-          failureMessages: [],
-        },
-        casualLoading: {
-          value: shift.casualLoading,
-          failureMessages: [],
-        },
-      };
+    var isValid = true;
+
+    this.shifts.map((shift, rowIndex) => {
+      const validators: [WorkerShiftColumnName, (value: string) => string[]][] = [
+        ['employeeCode', validateEmployeeCode],
+        ['lastName', validateLastName],
+        ['firstName', validateFirstName],
+      ];
+      
+      validators.forEach(([columnId, validator]) => {
+        const failureMessages = validator(shift[columnId]);
+        isValid = isValid && failureMessages.length === 0;
+
+        this.dispatch(setCellValidationMessages({
+          cellIdentifier: {
+            rowIndex,
+            columnId,
+          },
+          failureMessages: failureMessages,
+        }));
+      });
     });
+
+    return isValid;
   }
 }
 
 const validateEmployeeCode = (employeeCode: string): string[] => {
   if (employeeCode.length < 1) {
     return ['Employee code must have at least 1 character'];
-  } else if (!/$[a-zA-Z0-9]+^/.test(employeeCode)) {
+  } else if (!/^[a-zA-Z0-9]+$/.test(employeeCode)) {
     return ['Employee code must only consist of alphabetic (a-z or A-Z) and numeric (0-9) characters'];
   } else {
     return [];
@@ -79,4 +74,24 @@ const validateFirstName = (firstName: string): string[] => {
   }
 };
 
-export const useShiftTableValidator = () => new ShiftTableValidator();
+const workerShiftsSelector = (state: AppState): ValidatedWorkerShiftRow[] =>
+  state.shiftEntry.rows;
+
+const workerShiftsConverter = (rows: ValidatedWorkerShiftRow[]): WorkerShiftRow[] =>
+  rows.map((row) => ({
+    employeeCode: row.employeeCode.value,
+    lastName: row.lastName.value,
+    firstName: row.firstName.value,
+    basePayRate: row.basePayRate.value,
+    shiftStartDate: row.shiftStartDate.value,
+    shiftStartTime: row.shiftStartTime.value,
+    shiftEndTime: row.shiftEndTime.value,
+    casualLoading: row.casualLoading.value,
+  })).slice(0, rows.length - 1);
+
+export const useShiftTableValidator = () => {
+  const dispatch = useDispatch();
+  const shifts = useSelector(createSelector([workerShiftsSelector], workerShiftsConverter));
+
+  return new ShiftTableValidator(dispatch, shifts);
+};
