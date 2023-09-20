bce1e0287a74b5bc0cff6bc6c4a5175dbb549671
Samour <aaron.burke.cpp@gmail.com>
2023-09-20 20:47:23 +1000

    Add component for file upload

---
+++ award-calculator-fe/src/Components/FileSelect/index.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent, useRef } from 'react';
+
+interface FileSelectProps {
+  label: string;
+  className?: string;
+  onSelect: (file: File) => void;
+}
+
+const FileSelect = ({ label, className, onSelect }: FileSelectProps): JSX.Element => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    ref.current?.click();
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.currentTarget.files?.[0];
+    if (file) {
+      onSelect(file);
+    }
+  };
+
+  return (
+    <>
+      <input ref={ref} type="file" style={{ display: 'none' }} onChange={handleFileChange} />
+      <button className={className} onClick={handleClick}>{label}</button>
+    </>
+  )
+};
+
+export default FileSelect;
