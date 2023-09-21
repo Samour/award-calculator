c28d81eddead51fa12ea65043336a3491b933d37
Samour <aaron.burke.cpp@gmail.com>
2023-09-21 11:16:54 +1000

    Implement TimeClassifier

--- award-calculator-fe/src/award/TimeClassifier.ts
+++ award-calculator-fe/src/award/TimeClassifier.ts
@@ -1,9 +1,76 @@
-import { ZonedDateTime } from '@js-joda/core';
+import { Duration, ZonedDateTime } from '@js-joda/core';
+import Decimal from 'decimal.js';
 import { WorkerShift } from 'models/inputs/shift';
 import { IncrementalMinuteDuration, ShiftTimestamp } from 'models/time';
 
-export interface TimeClassifier {
-  classifyShift(shift: WorkerShift): ClassifiedWorkedTime[];
+export class TimeClassifier {
+
+  constructor(private readonly overtimeCounters: OvertimeCounter[]) { }
+
+  classifyShift(shift: WorkerShift): ClassifiedWorkedTime[] {
+    const overtimeSpans = this.overtimeCounters.map((counter) => counter.countOvertimeInShift(shift))
+      .filter((span): span is OvertimeSpan => !!span);
+    overtimeSpans.sort((a, b) => a.startTime.compareTo(b.startTime));
+
+    const mergedOvertimeSpans = this.mergeSpans(overtimeSpans);
+
+    const classifiedTime: ClassifiedWorkedTime[] = [];
+    let unclassifiedStart = shift.startTime;
+    mergedOvertimeSpans.forEach((span) => {
+      if (unclassifiedStart.isBefore(span.startTime)) {
+        classifiedTime.push({
+          startTime: unclassifiedStart,
+          endTime: span.startTime,
+          duration: new Decimal(Duration.between(unclassifiedStart, span.startTime).toMinutes()),
+          classification: WorkTimeClassification.REGULAR_TIME,
+        });
+      }
+      
+      classifiedTime.push({
+        startTime: span.startTime,
+        endTime: span.endTime,
+        duration: new Decimal(Duration.between(span.startTime, span.endTime).toMinutes()),
+        classification: WorkTimeClassification.OVERTIME,
+      });
+      unclassifiedStart = span.endTime;
+    });
+
+    if (unclassifiedStart.isBefore(shift.endTime)) {
+      classifiedTime.push({
+        startTime: unclassifiedStart,
+        endTime: shift.endTime,
+        duration: new Decimal(Duration.between(unclassifiedStart, shift.endTime).toMinutes()),
+        classification: WorkTimeClassification.REGULAR_TIME,
+      });
+    }
+
+    return classifiedTime;
+  }
+
+  private mergeSpans(overtimeSpans: OvertimeSpan[]): OvertimeSpan[] {
+    const mergedSpans: OvertimeSpan[] = [];
+    let currentSpan: OvertimeSpan | null = null;
+
+    overtimeSpans.forEach((span) => {
+      if (!currentSpan) {
+        currentSpan = span;
+      } else if (currentSpan.endTime.isBefore(span.startTime)) {
+        mergedSpans.push(currentSpan);
+        currentSpan = span;
+      } else {
+        currentSpan = {
+          startTime: currentSpan.startTime,
+          endTime: span.endTime,
+        };
+      }
+    });
+
+    if (currentSpan) {
+      mergedSpans.push(currentSpan);
+    }
+
+    return mergedSpans;
+  }
 }
 
 export enum WorkTimeClassification {
