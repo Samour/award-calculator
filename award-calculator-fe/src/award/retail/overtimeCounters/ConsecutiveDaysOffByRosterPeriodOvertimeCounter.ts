d392c182cb177dc198fe0e0011a3ccb0449670be
Samour <aaron.burke.cpp@gmail.com>
2024-05-28 07:42:56 +1000

    Finalize representation of fortnights

--- award-calculator-fe/src/award/retail/overtimeCounters/ConsecutiveDaysOffByRosterPeriodOvertimeCounter.ts
+++ award-calculator-fe/src/award/retail/overtimeCounters/ConsecutiveDaysOffByRosterPeriodOvertimeCounter.ts
@@ -1,41 +1,71 @@
-import { DateTimeFormatter, ZonedDateTime } from '@js-joda/core';
+import { DateTimeFormatter, DayOfWeek, LocalDate, TemporalAdjusters } from '@js-joda/core';
 import { LookAheadOvertimeCounter } from 'award/TimeClassifier';
 import { WorkerShift } from 'models/inputs/shift';
 import { OvertimeReason } from 'models/outputs/payable';
 import { ShiftTimestamp, TimeSpan } from 'models/time';
 
 interface RosterPeriodShifts {
-  // Format: YYYY-MM-DD of the first Monday of the fortnight
-  rosterPeriod: string;
-  // Ordered list of dates (YYYY-MM-DD format) that have a shift on that day
-  shiftDates: string[];
+  // Date of the first Monday of the fortnight
+  rosterPeriod: LocalDate;
+  // Ordered list of dates that have a shift on that day
+  shiftDates: LocalDate[];
 }
 
 export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAheadOvertimeCounter {
 
-  // Date in YYYY-FF format, where YYYY is the weekyear & FF is the fortnight number
+  private currentPeriod?: RosterPeriodShifts;
+  // Date in YYYY-MM-DD format
   private readonly overtimeDays: Set<string> = new Set();
 
   public readonly reason: OvertimeReason = OvertimeReason.CONSECUTIVE_DAYS_OFF;
 
   public peekShift(shift: WorkerShift): void {
-    // console.log(shift);
+    const fortnightStart = firstDateOfFortnight(shift.startTime);
     console.log({
       date: toDate(shift.startTime),
       isoWeekyear: shift.startTime.toLocalDate().isoWeekyear(),
       isoWeek: shift.startTime.toLocalDate().isoWeekOfWeekyear(),
-      isoFortnight: toFortnightKey(shift.startTime),
+      fortnightStart: fortnightStart.format(DateTimeFormatter.ISO_LOCAL_DATE),
     });
+
+    // Assumption: Shift does not cross days
+    const rosterPeriod = firstDateOfFortnight(shift.startTime);
+    if (!this.currentPeriod?.rosterPeriod?.equals(rosterPeriod)) {
+      this.finalizeCurrentPeriod();
+      this.currentPeriod = {
+        rosterPeriod,
+        shiftDates: [],
+      };
+    }
+
+    if (this.currentPeriod.shiftDates[this.currentPeriod.shiftDates.length - 1]?.equals(shift.startTime.toLocalDate())) {
+      this.currentPeriod.shiftDates.push(shift.startTime.toLocalDate());
+    }
   }
 
   public countOvertimeInShift(shift: WorkerShift): TimeSpan[] {
     return [];
   }
+
+  private finalizeCurrentPeriod(): void {
+    if (!this.currentPeriod) {
+      return;
+    }
+    // TODO update this.overtimeDays
+
+    this.currentPeriod = undefined;
+  }
 }
 
 const toDate = (ts: ShiftTimestamp): string => ts.format(DateTimeFormatter.ISO_LOCAL_DATE);
 
-const toFortnightKey = (ts: ShiftTimestamp): string => {
-  const date = ts.toLocalDate();
-  return `${date.isoWeekyear()}-${Math.ceil(date.isoWeekOfWeekyear() / 2)}`;
+// We need to use the full date because js-jode doesn't expose methods to convert from
+// ISOWeekYear attributes -> LocalDate
+const firstDateOfFortnight = (ts: ShiftTimestamp): LocalDate => {
+  const date = ts.toLocalDate().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+  if (date.isoWeekOfWeekyear() % 2 === 0) {
+    return date.minusWeeks(1);
+  } else {
+    return date;
+  }
 };
