c03a5867f2fe9fa6002e2f69f9ed3bbf320bb403
Samour <aaron.burke.cpp@gmail.com>
2024-05-28 08:37:52 +1000

    Implement calculation of weeks & dates that should be OT due to rostered day gap rule

--- award-calculator-fe/src/award/retail/overtimeCounters/ConsecutiveDaysOffByRosterPeriodOvertimeCounter.ts
+++ award-calculator-fe/src/award/retail/overtimeCounters/ConsecutiveDaysOffByRosterPeriodOvertimeCounter.ts
@@ -1,4 +1,4 @@
-import { DateTimeFormatter, DayOfWeek, LocalDate, TemporalAdjusters } from '@js-joda/core';
+import { DateTimeFormatter, DayOfWeek, Duration, LocalDate, TemporalAdjusters } from '@js-joda/core';
 import { LookAheadOvertimeCounter } from 'award/TimeClassifier';
 import { WorkerShift } from 'models/inputs/shift';
 import { OvertimeReason } from 'models/outputs/payable';
@@ -20,7 +20,7 @@ export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAhea
   public readonly reason: OvertimeReason = OvertimeReason.CONSECUTIVE_DAYS_OFF;
 
   public peekShift(shift: WorkerShift): void {
-    const fortnightStart = firstDateOfFortnight(shift.startTime);
+    const fortnightStart = firstDateOfFortnight(shift.startTime.toLocalDate());
     console.log({
       date: toDate(shift.startTime),
       isoWeekyear: shift.startTime.toLocalDate().isoWeekyear(),
@@ -29,7 +29,7 @@ export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAhea
     });
 
     // Assumption: Shift does not cross days
-    const rosterPeriod = firstDateOfFortnight(shift.startTime);
+    const rosterPeriod = firstDateOfFortnight(shift.startTime.toLocalDate());
     if (!this.currentPeriod?.rosterPeriod?.equals(rosterPeriod)) {
       this.finalizeCurrentPeriod();
       this.currentPeriod = {
@@ -38,8 +38,10 @@ export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAhea
       };
     }
 
-    if (this.currentPeriod.shiftDates[this.currentPeriod.shiftDates.length - 1]?.equals(shift.startTime.toLocalDate())) {
-      this.currentPeriod.shiftDates.push(shift.startTime.toLocalDate());
+    const currentDate = shift.startTime.toLocalDate();
+    const mostRecentSeenDate = this.currentPeriod.shiftDates[this.currentPeriod.shiftDates.length - 1];
+    if (!mostRecentSeenDate?.equals(currentDate)) {
+      this.currentPeriod.shiftDates.push(currentDate);
     }
   }
 
@@ -53,7 +55,50 @@ export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAhea
     }
     // TODO update this.overtimeDays
 
+    const weeksWithoutBreak = new Set([1, 2]);
+    let dateItr = this.currentPeriod.rosterPeriod.minusDays(1);
+    let weekNo = 1;
+    for (let breakBoundary of this.datesToCalculateBreaks()) {
+      if (breakBoundary.dayOfWeek().equals(DayOfWeek.MONDAY)
+        && !breakBoundary.equals(this.currentPeriod.rosterPeriod)) {
+        weekNo++;
+      }
+
+      const breakLength = Duration.between(dateItr, breakBoundary).toDays() - 1;
+      if (breakLength >= 3) {
+        weeksWithoutBreak.delete(1);
+        weeksWithoutBreak.delete(2);
+        break;
+      } else if (breakLength === 2) {
+        weeksWithoutBreak.delete(weekNo);
+        if (weekNo === 2) {
+          break;
+        }
+      }
+
+      dateItr = breakBoundary;
+    }
+
+    const otDays = weeksWithoutBreakToOTDays(weeksWithoutBreak.size);
+    for (let i = otDays; i > 0; i--) {
+      const otDay = this.currentPeriod.shiftDates[this.currentPeriod.shiftDates.length - i];
+      this.overtimeDays.add(otDay.format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+
     this.currentPeriod = undefined;
+  }
+
+  private datesToCalculateBreaks(): LocalDate[] {
+    if (!this.currentPeriod) {
+      return [];
+    }
+
+    const dates = [...this.currentPeriod.shiftDates];
+    // Call firstDateOfFortnight in case we're looking at a 1-week fortnight in a leap ISOWeekYear
+    const startOfNextFortnight = firstDateOfFortnight(this.currentPeriod.rosterPeriod.plusWeeks(2));
+    dates.push(startOfNextFortnight);
+
+    return dates;
   }
 }
 
@@ -61,11 +106,19 @@ const toDate = (ts: ShiftTimestamp): string => ts.format(DateTimeFormatter.ISO_L
 
 // We need to use the full date because js-jode doesn't expose methods to convert from
 // ISOWeekYear attributes -> LocalDate
-const firstDateOfFortnight = (ts: ShiftTimestamp): LocalDate => {
-  const date = ts.toLocalDate().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
-  if (date.isoWeekOfWeekyear() % 2 === 0) {
-    return date.minusWeeks(1);
+const firstDateOfFortnight = (date: LocalDate): LocalDate => {
+  const startOfWeek = date.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+  if (startOfWeek.isoWeekOfWeekyear() % 2 === 0) {
+    return startOfWeek.minusWeeks(1);
   } else {
-    return date;
+    return startOfWeek;
+  }
+};
+
+const weeksWithoutBreakToOTDays = (weeksWithoutBreak: number): number => {
+  switch (weeksWithoutBreak) {
+    case 2: return 3;
+    case 1: return 1;
+    default: return 0;
   }
 };
