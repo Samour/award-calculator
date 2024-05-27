1e5d4c8ed907d4b8520c801359ec2722614743ad
Samour <aaron.burke.cpp@gmail.com>
2024-05-28 06:00:42 +1000

    Switch back to using ISO weekyear for fortnight calculation
    Rather than home-rolled implementation

--- award-calculator-fe/src/award/retail/overtimeCounters/ConsecutiveDaysOffByRosterPeriodOvertimeCounter.ts
+++ award-calculator-fe/src/award/retail/overtimeCounters/ConsecutiveDaysOffByRosterPeriodOvertimeCounter.ts
@@ -2,7 +2,7 @@ import { DateTimeFormatter, ZonedDateTime } from '@js-joda/core';
 import { LookAheadOvertimeCounter } from 'award/TimeClassifier';
 import { WorkerShift } from 'models/inputs/shift';
 import { OvertimeReason } from 'models/outputs/payable';
-import { ShiftTimestamp, TimeSpan, toWeekOfYear } from 'models/time';
+import { ShiftTimestamp, TimeSpan } from 'models/time';
 
 interface RosterPeriodShifts {
   // Format: YYYY-MM-DD of the first Monday of the fortnight
@@ -13,15 +13,19 @@ interface RosterPeriodShifts {
 
 export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAheadOvertimeCounter {
 
-  // Date in YYYY-MM-DD format
+  // Date in YYYY-FF format, where YYYY is the weekyear & FF is the fortnight number
   private readonly overtimeDays: Set<string> = new Set();
 
   public readonly reason: OvertimeReason = OvertimeReason.CONSECUTIVE_DAYS_OFF;
 
   public peekShift(shift: WorkerShift): void {
-    console.log(shift);
-    console.log(toDate(shift.startTime));
-    console.log(toWeekOfYear(shift.startTime));
+    // console.log(shift);
+    console.log({
+      date: toDate(shift.startTime),
+      isoWeekyear: shift.startTime.toLocalDate().isoWeekyear(),
+      isoWeek: shift.startTime.toLocalDate().isoWeekOfWeekyear(),
+      isoFortnight: toFortnightKey(shift.startTime),
+    });
   }
 
   public countOvertimeInShift(shift: WorkerShift): TimeSpan[] {
@@ -31,7 +35,7 @@ export class ConsecutiveDaysOffByRosterPeriodOvertimeCounter implements LookAhea
 
 const toDate = (ts: ShiftTimestamp): string => ts.format(DateTimeFormatter.ISO_LOCAL_DATE);
 
-// const toStartOfFortnight = (ts: ShiftTimestamp): ZonedDateTime => {
-//   const firstDayOfWeek = ts.minusDays(ts.dayOfWeek().ordinal() - 1);
-//   const weekNumber = Math.floor(firstDayOfWeek.dayOfYear() / 7);
-// };
+const toFortnightKey = (ts: ShiftTimestamp): string => {
+  const date = ts.toLocalDate();
+  return `${date.isoWeekyear()}-${Math.ceil(date.isoWeekOfWeekyear() / 2)}`;
+};
